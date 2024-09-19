0bb22d5a9fbb74c5db4156e54561bf1e12bb82ba
akachancy <toribiochristianalpar905@gmail.com>
2024-09-19 15:12:58 +0800

    update game_list.py

--- game_list.py
+++ game_list.py
@@ -1,4 +1,4 @@
-video_game_titles = ["The Legend of Zelda: Breath of the Wild", "Grand Theft Auto V", "Red Dead Redemption 2", "The Witcher 3: Wild Hunt", "Super Mario Odyssey", "God of War", "Horizon Zero Dawn", "Elden Ring", "The Last of Us", "Mario Kart 8 Deluxe", "Animal Crossing: New Horizons", "Super Smash Bros. Ultimate", "Portal 2", "Half-Life 2", "The Elder Scrolls V: Skyrim"]
+video_game_titles = ["The Legend of Zelda: Breath of the Wild", "Grand Theft Auto V", "Red Dead Redemption 2", "The Witcher 3: Wild Hunt", "Super Mario Odyssey", "God of War", "Horizon Zero Dawn", "Elden Ring", "The Last of Us", "Mario Kart 8 Deluxe", "Animal Crossing: New Horizons", "Super Smash Bros. Ultimate", "Portal 2", "Half-Life 2", "Mobile Legends Bangbang"]
 
 print(video_game_titles)
 
