216c2a61177a8ac370dde49f0b7a6cae6f25cb5a
akachancy <toribiochristianalpar905@gmail.com>
2024-09-19 15:20:12 +0800

    update movie_list.py

--- movie_list.py
+++ movie_list.py
@@ -1,4 +1,4 @@
-movie_titles = ["The Shawshank Redemption", "The Godfather", "The Godfather: Part II", "Schindler's List", "12 Angry Men", "Pulp Fiction", "The Lord of the Rings: The Return of the King", "The Good, the Bad and the Ugly", "Fight Club", "The Dark Knight", "The Lord of the Rings: The Fellowship of the Ring", "The Lord of the Rings: The Two Towers", "Forrest Gump", "The Silence of the Lambs", "Inception", "Saving Private Ryan", "Life is Beautiful", "The Green Mile", "The Matrix", "The Dark Knight Rises"]
+movie_titles = ["The Shawshank Redemption", "The Godfather", "The Godfather: Part II", "Schindler's List", "12 Angry Men", "Pulp Fiction", "The Lord of the Rings: The Return of the King", "The Good, the Bad and the Ugly", "Fight Club", "The Dark Knight", "The Lord of the Rings: The Fellowship of the Ring", "The Lord of the Rings: The Two Towers", "Forrest Gump", "The Silence of the Lambs", "Inception", "Saving Private Ryan", "Life is Beautiful", "The Green Mile", "The Matrix", "Hello, Love, Goodbye"]
 
 print(movie_titles)
 
